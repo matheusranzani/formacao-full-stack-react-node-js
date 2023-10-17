b810353b9ccd8288d8c58078256c76e19397cad8
Matheus Ranzani <ranzani.matheus@gmail.com>
2023-10-17 18:46:14 -0300

    Primeiro commit

---
+++ alura-books/src/componentes/IconesHeader/index.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+import perfil from '../../imagens/perfil.svg'
+import sacola from '../../imagens/sacola.svg';
+
+const icones = [perfil, sacola];
+
+const Icones = styled.ul`
+  display: flex;
+  align-items: center;
+`;
+
+const Icone = styled.li`
+  margin-right: 40px;
+  width: 25px;
+`;
+
+function IconesHeader() {
+  return (
+    <Icones>
+      { icones.map((icone) => (
+        <Icone>
+          <img src={icone} alt='icone'/>
+        </Icone>
+      )) }
+    </Icones>
+  );
+}
+
+export default IconesHeader;
---
+++ alura-books/src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -0,0 +1,7 @@
+import livro from '../../imagens/livro.png'
+
+export const livros = [
+  {nome: 'Liderança em Design', id: 1, src: livro},
+  {nome: 'Cangaceiro JavaScript', id: 3, src: livro},
+  {nome: 'Aventureiros do Java', id: 4, src: livro}
+]
